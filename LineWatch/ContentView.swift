bdc809c6b56afeb7628ce13389be7d6c1a5656fb
steven-nguyen22 <steve.huu.nguyen@gmail.com>
2024-11-10 17:11:32 -0800

    Initial Commit

---
+++ LineWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LineWatch
+//
+//  Created by Steven Nguyen on 11/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LineWatch/LineWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineWatchApp.swift
+//  LineWatch
+//
+//  Created by Steven Nguyen on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LineWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
